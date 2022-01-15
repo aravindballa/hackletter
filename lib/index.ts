efb9915b77eeac436e00a1d6a933d15a2577cb9a
Aravind Balla <bsaaravind@gmail.com>
2022-01-15 16:28:15 +0530

    feat: cache rss fetch

--- lib/index.ts
+++ lib/index.ts
@@ -6,7 +6,9 @@ import { Feed } from "../types";
 const HACKLETTER_RSS = "https://buttondown.email/aravindballa/rss";
 
 export const hackletterPosts = async (): Promise<Feed | null> => {
-  const rssContent = await (await fetch(HACKLETTER_RSS)).text();
+  const rssContent = await (
+    await fetch(HACKLETTER_RSS, { cf: { cacheTtl: 60, cacheEverything: true } })
+  ).text();
   const feed = await parseFeed(rssContent);
 
   if (!feed) return null;
